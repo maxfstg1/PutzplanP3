7d0a607a029e8c7c9b4c685c893808656bbc2ea4
maxfstg1 <max.festag@gmail.com>
2025-11-20 13:42:37 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,19 @@
-const CACHE_NAME = "pwacache-v1";
-
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) =>
-      cache.addAll(["./index.html", "./manifest.json", "./icon.png"])
-    )
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
+    caches.open("pwacache").then(cache =>
+      cache.addAll([
+        "./index.html",
+        "./script.js",
+        "./firebase.js",
+        "./manifest.json",
+        "./icon.png"
+      ])
     )
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
