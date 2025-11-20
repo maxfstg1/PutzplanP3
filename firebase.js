0c298dc9df3dd737b0727501283aca3408db3211
maxfstg1 <max.festag@gmail.com>
2025-11-20 13:39:05 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyAtyYZQcZ2IogYAI4fQKQl0dvXlKrkIatk",
+    authDomain: "putzplan-36a5a.firebaseapp.com",
+    projectId: "putzplan-36a5a",
+    storageBucket: "putzplan-36a5a.firebasestorage.app",
+    messagingSenderId: "508349984150",
+    appId: "1:508349984150:web:459ca21ee4d70203d401b7",
+    measurementId: "G-F077CG9YKW"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
